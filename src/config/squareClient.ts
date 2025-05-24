69dc18850c4858f88f67c86607ca86bad37e10da
AryanPahuja21 <aryanpahuja1421@gmail.com>
2025-05-24 19:33:57 +0530

    square client setup

---
+++ src/config/squareClient.ts
@@ -0,0 +1,34 @@
+const { Client, environments } = require("square");
+require("dotenv").config();
+
+const client = new Client({
+  accessToken: process.env.SQUARE_ACCESS_TOKEN,
+  environment: environments.sandbox,
+});
+
+async function getLocations() {
+  try {
+    const response = await client.locationsApi.listLocations();
+    const locations = response.result.locations;
+
+    if (locations && locations.length > 0) {
+      locations.forEach(
+        (location: {
+          id: any;
+          name: any;
+          address: { addressLine1: any; locality: any };
+        }) => {
+          console.log(
+            `${location.id}: ${location.name}, ${location.address?.addressLine1}, ${location.address?.locality}`
+          );
+        }
+      );
+    } else {
+      console.log("No locations found.");
+    }
+  } catch (error) {
+    console.error("Error fetching locations:", error);
+  }
+}
+
+getLocations();
